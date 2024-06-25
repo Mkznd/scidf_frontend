70a98280fd17f482565ec59e00d14c5c6300482f
Mkznd <yzundai1@gmail.com>
2024-06-25 19:21:35 +0200

    Established connection with backend

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const Search = () => {
     const [searchValue, setSearchValue] = useState('');
-    
-    const handleSearch = (e: React.FormEvent) => {
+
+    function handleSearch(e: React.FormEvent) {
         e.preventDefault();
+        const a = fetch(`http://localhost:8000/`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error));
         console.log(searchValue);
-    };
-    
+    }
+
     return (
         <form onSubmit={handleSearch} className={'flex justify-center gap-2'}>
-        <input
-            type="text"
-            placeholder='Search...'
-            value={searchValue}
-            className={"border w-1/2 p-2"}
-            onChange={(e) => setSearchValue(e.target.value)}
-        />
-        <button className={"border p-2"} type="submit">Search articles</button>
+            <input
+                type="text"
+                placeholder='Search...'
+                value={searchValue}
+                className={"border w-1/2 p-2"}
+                onChange={(e) => setSearchValue(e.target.value)}
+            />
+            <button className={"border p-2"} type="submit">Search articles</button>
         </form>
     );
-    }
+}
 
 export default Search;
